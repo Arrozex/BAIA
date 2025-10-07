3aee158e09ad2eec5e22deacfdecd3869d0f308a
WEI <111346524+Arrozex@users.noreply.github.com>
2025-10-07 16:13:54 +0800

    Update events.js

--- api/events.js
+++ api/events.js
@@ -1,6 +1,6 @@
 module.exports = async function handler(req, res) {
   const API_KEY = process.env.GOOGLE_API_KEY;   // 從 Vercel 環境變數讀取
-  const CALENDAR_ID = "https://calendar.google.com/calendar/ical/aforgame77%40gmail.com/private-ccecd1665f46bc62c490d0609590e89e/basic.ics";   // 你的日曆 ID
+  const CALENDAR_ID = "https://calendar.google.com/calendar/ical/aforgame77%40gmail.com/public/basic.ics";   // 你的日曆 ID
 
   const timeMin = new Date().toISOString();
   const timeMax = new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toISOString();
